96463cc517fd69ac59e71facffde78299f40a5cc
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-03-16 17:09:24 -0500

    Update calculator.js

--- forms/calculator/calculator.js
+++ forms/calculator/calculator.js
@@ -3,6 +3,21 @@ add.onclick=function(){
   let name = nameINPT.value
   let num1 = Number(n1.value)
   let num2 = Number(n2.value)
+  
   let result = num1 + num2
   label2.value = (name + ", the sum is " + result)
+}
+
+mult.onclick=function(){
+  let name = nameINPT.value
+  let num1 = Number(n1.value)
+  let num2 = Number(n2.value)
+  
+  let result = num1 * num2
+  label2.value = (name + ", the product is " + result)
+}
+clear.onclick=function(){
+  label2.value = ""
+  n1.value= ""
+  n2.value= ""
 }
