187c1fceeb1ced42935cd76ed2cee0bdbfa07660
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-03-16 17:01:32 -0500

    Update calculator.js

--- forms/calculator/calculator.js
+++ forms/calculator/calculator.js
@@ -4,5 +4,5 @@ add.onclick=function(){
   let num1 = Number(n1.value)
   let num2 = Number(n2.value)
   let result = num1 + num2
-  label2.value = `${name}, the sum is ${result}`
+  label2.value = (name + ", the sum is " + result)
 }
