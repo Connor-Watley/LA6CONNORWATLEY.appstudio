0b6cbeb6de4ee09abcd0edcf669b50cbfd37e975
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-03-16 17:02:26 -0500

    Update membership.js

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -3,7 +3,7 @@ Button1.onclick=function(){
   let userName = input1.value
   let isMember = members.includes(userName);
   if(isMember == true){
-    let label1.value = userName;
+    let label1.value = userName
 } else {
     members.push(userName)
     let label1.value = "you are not a member"
