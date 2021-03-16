83a2b5c4f5cb25e0e54f3115dff8b18f17a4bf65
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-03-16 18:39:43 -0500

    Update videos.js

--- forms/videos/videos.js
+++ forms/videos/videos.js
@@ -1,11 +1,12 @@
 
 Select1.onchange=function(){
-    if(){
-  
-        YouTube2.videoID = "2g7pavm9bbs"
+    if(items = "Programming Video"){
+        YouTube2.videoID = '2g7pavm9bbs'
+    } else {
+        YouTube2.videoID = 'b43PbeSekDo'
+        }
+        }
+        
+
+       
     
-}else {
-        YouTube2.videoID = "b43PbeSekDo"
-        } 
-    
-}
