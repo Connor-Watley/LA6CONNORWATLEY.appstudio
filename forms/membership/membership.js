16ece4cd840a4722c642e5c7a78b4ee2563be93d
Connor Watley <62679869+Connor-Watley@users.noreply.github.com>
2021-03-16 16:49:03 -0500

    Create membership.js

---
+++ forms/membership/membership.js
@@ -0,0 +1,11 @@
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+Button1.onclick=function(){
+  let userName = input1.value
+  let isMember = members.includes(userName);
+  if(isMember == true){
+    let label1.value = userName;
+} else {
+    members.push(userName)
+    let label1.value = "you are not a member"
+    }
+}
